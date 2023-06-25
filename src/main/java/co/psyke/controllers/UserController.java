f1ae811bba57972e5eb08a43c78d10291a7e65bc
PsykeDady <psdady@msn.com>
2023-06-26 01:37:35 +0200

    edit fixed

--- src/main/java/co/psyke/controllers/UserController.java
+++ src/main/java/co/psyke/controllers/UserController.java
@@ -15,7 +15,6 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -55,7 +54,7 @@ public class UserController {
 		return new ResponseEntity<Void>(HttpStatus.NO_CONTENT);
 	}
 	
-	@PutMapping("update")
+	@PostMapping("update")
 	public ResponseEntity<Boolean> updateUser(@RequestBody @Valid User user){
 		Boolean updated = false;  
 		try {
