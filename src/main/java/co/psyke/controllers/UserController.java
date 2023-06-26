69857aad05bdf31b0e716ca8971023671949399d
PsykeDady <psdady@msn.com>
2023-06-26 02:14:30 +0200

    restored put

--- src/main/java/co/psyke/controllers/UserController.java
+++ src/main/java/co/psyke/controllers/UserController.java
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -54,7 +55,7 @@ public class UserController {
 		return new ResponseEntity<Void>(HttpStatus.NO_CONTENT);
 	}
 	
-	@PostMapping("update")
+	@PutMapping("update")
 	public ResponseEntity<Boolean> updateUser(@RequestBody @Valid User user){
 		Boolean updated = false;  
 		try {
